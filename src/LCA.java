05b30b8b3e21877d74f7bf052bf756bc3fbcf6d9
senroche <35078186+senroche@users.noreply.github.com>
2019-10-09 14:11:55 +0100

    Set up Binary Tree

---
+++ src/LCA.java
@@ -0,0 +1,31 @@
+// Java Program that finds LCA in a Binary Tree.
+import java.util.ArrayList; 
+import java.util.List; 
+
+public class LCA{
+// Binary Tree Node
+class Node { 
+    int data; 
+    Node left, right; 
+  
+    Node(int value) { 
+        data = value; 
+        left = null;
+        right = null;
+    } 
+} 
+  
+public class BT_LCA 
+{ 
+  
+    Node root; 
+    
+    } 
+  
+    
+    public static void main(String[] args) 
+    { 
+       
+      
+    } 
+} 
