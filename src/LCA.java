d86f6656afc4f87062d08c60315eb6530ee8bb55
senroche <rochesean@gmail.com>
2019-10-10 15:24:35 +0100

    Added empty methods and LCA test
    Adding some tests before finishing LCA

--- src/LCA.java
+++ src/LCA.java
@@ -1,10 +1,13 @@
 // Java Program that finds LCA in a Binary Tree.
+//O(n) solution to find LCA of two given nodes n1 and n2
+
 import java.util.ArrayList; 
 import java.util.List; 
 
 public class LCA{
+	
 // Binary Tree Node
-class Node { 
+public static class Node { 
     int data; 
     Node left, right; 
   
@@ -14,18 +17,36 @@ class Node {
         right = null;
     } 
 } 
-  
-public class BT_LCA 
+
+//Binary tree class for LCA with no parent pointer.
+public static class BT_NoParentPointer 
 { 
-  
     Node root; 
-    
-    } 
-  
-    
-    public static void main(String[] args) 
-    { 
-       
-      
-    } 
-} 
+    private List<Integer> path1 = new ArrayList<>();
+    private List<Integer> path2 = new ArrayList<>();
+     
+
+
+	// Finds LCA will utilise an internal function that calls findPath
+	int findLCA(int n1, int n2) {
+		return 0;
+	}
+	
+		
+	    
+	// Finds the path from node to the given root of the tree, Stores the
+    // path in a vector path[], returns true if path exists otherwise false
+    private boolean findPath(Node root, int n, List<Integer> path) {
+        
+
+        return false;
+    }
+
+
+}
+
+public static void main(String[] args) {
+
+
+}
+}
---
+++ src/LCATest.java
@@ -0,0 +1,34 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class LCATest {
+
+	
+	// Test LCA BT[4,2,6,1,3,5,7]
+    @Test
+    public void test() {
+    	LCA.BT_NoParentPointer tree = new LCA.BT_NoParentPointer();
+        tree.root = new LCA.Node(4); tree.root.left = new LCA.Node(2);
+        tree.root.right = new LCA.Node(6); tree.root.left.left = new LCA.Node(1);
+        tree.root.left.right = new LCA.Node(3); tree.root.right.left = new LCA.Node(5);
+        tree.root.right.right = new LCA.Node(7);
+        /*
+        
+        BST Diagram:
+        	  4
+			/   \
+		   2     6
+		  / \   / \
+		 1	 3 5   7
+
+        */
+
+        assertEquals( 1,tree.findLCA(2, 6));
+        assertEquals( 2,tree.findLCA(1, 3));
+        assertEquals( 6,tree.findLCA(5, 7));
+        assertEquals( 4,tree.findLCA(1, 6));
+        assertEquals( 4,tree.findLCA(7, 2));
+        assertEquals( 4,tree.findLCA(3, 5));
+    }
+}
